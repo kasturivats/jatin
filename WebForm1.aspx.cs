51d61742cc603894860e23691f71587ff5a455fb
kasturivats <40772517+kasturivats@users.noreply.github.com>
2021-11-30 21:32:21 +0530

    Add files via upload

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace j
+{
+
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string cs = ConfigurationManager.ConnectionStrings["dbvac"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+        protected void ButtonSearch_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select * from vaccine where aadhar = '" + TextSearch.Text + "'", con);
+                cmd.ExecuteNonQuery();
+                DataTable dt = new DataTable();
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(dt);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+        }
+        protected void GVbind()
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select * from vaccine", con);
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows == true)
+                {
+                    GridView1.DataSource = dr;
+                    GridView1.DataBind();
+                }
+            }
+
+
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            GVbind();
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            int id = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value.ToString());
+            string name = ((TextBox)GridView1.Rows[e.RowIndex].Cells[0].Controls[0]).Text.ToString();
+            string aadhar = ((TextBox)GridView1.Rows[e.RowIndex].Cells[1].Controls[0]).Text.ToString();
+            string vaccine = ((TextBox)GridView1.Rows[e.RowIndex].Cells[2].Controls[0]).Text.ToString();
+            CheckBox dose1 = (CheckBox)GridView1.Rows[e.RowIndex].Cells[3].FindControl("CheckBox1");
+            CheckBox dose2 = (CheckBox)GridView1.Rows[e.RowIndex].Cells[4].FindControl("CheckBox2");
+
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(@"UPDATE vaccine
+   SET name = '" + name + "',aadhar = '" + aadhar + "',vaccine = '" + vaccine + "', dose1 = '" + dose1 + "' WHERE Id='" + id + "'", con);
+                int t = cmd.ExecuteNonQuery();
+                if (t > 0)
+                {
+                    Response.Write("Updated");
+                    GridView1.EditIndex = -1;
+                    GVbind();
+                }
+            }
+
+
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+
+            int id = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value.ToString());
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(@"DELETE FROM vaccine WHERE id='" + id + "'", con);
+                int t = cmd.ExecuteNonQuery();
+                if (t > 0)
+                {
+                    Response.Write("Deleted");
+                    GridView1.EditIndex = -1;
+                    GVbind();
+                }
+            }
+        }
+    }
+}
